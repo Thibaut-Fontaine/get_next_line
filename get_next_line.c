202a14ba0e419b5c33f20cb8757a87f0e9f9017e
Thibaut FONTAINE <tfontain@e2r3p2.42.fr>
2017-01-03 06:04:32 +0100

    new main func but dont work

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 16:49:40 by tfontain          #+#    #+#             */
-/*   Updated: 2016/12/31 05:16:29 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/01/03 06:04:04 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 /*
 ** cherche le prochain newline dans une string et renvoie son emplacement
+** commencant donc a 0, puis 1, 2, 3, etc...
 ** si il n'y a pas de newline dans la string, renvoie -1
 */
 
@@ -33,7 +34,7 @@ int					ft_find_nl(char *s)
 ** - si str n'est pas NULL : malloc de size, copie size caracteres de str 
 ** dans le pt obtenu, NUL-terminate le pt, puis free str
 ** - si un malloc foire, renvoie NULL
-** - si str est NULL, renvoie malloc de size
+** - si str est NULL, renvoie malloc de size et le memset a un ascii lambda
 ** ! size ne doit pas compter le 0
 */
 
@@ -45,7 +46,7 @@ char				*ft_realloc_str(char *str, size_t size)
 	{
 		if ((r = malloc(size + 1)) == NULL)
 			return (NULL);
-		ft_memcpy(r, str, size);
+		ft_strncpy(r, str, size);
 		r[size] = 0;
 		free(str);
 	}
@@ -53,6 +54,7 @@ char				*ft_realloc_str(char *str, size_t size)
 	{
 		if ((r = malloc(size + 1)) == NULL)
 			return (NULL);
+		ft_bzero(r, size);
 	}
 	return (r);
 }
@@ -96,12 +98,63 @@ t_endl				*ft_get_current(const int fd, t_endl *current)
 	return (current);
 }
 
-/*
-** je considere que line a deja ete alloue
-*/
 // si un malloc foire renvoyer -1 !
 
 int					get_next_line(const int fd, char **line)
+{
+	static t_endl	*current = NULL;
+	unsigned int	len;
+	int				nl;
+	int				r;
+	t_endl			*tmp;
+
+	current = ft_get_current(fd, current);
+	if (current->s != NULL)
+	{
+		if ((*line =
+					malloc((len = ft_strlen(current->s)) + BUFF_SIZE)) == NULL)
+			return (-1);
+		ft_strcpy(*line, current->s);
+		ft_memdel((void**)&(current->s));
+	}
+	else
+	{
+		if ((*line = malloc(BUFF_SIZE)) == NULL)
+			return (-1);
+		len = BUFF_SIZE;
+	}
+	while ((r = read(fd, *line + len - BUFF_SIZE, BUFF_SIZE)) == 1)
+	{
+		if ((nl = ft_find_nl(*line)) != -1)
+		{
+			if ((current->s = malloc(nl + 1)) == NULL)
+				return (-1);
+			ft_strcpy(current->s, *line + len - BUFF_SIZE);
+			return (1);
+		}
+		if ((*line = ft_realloc_str(*line, len + BUFF_SIZE)) == NULL)
+			return (-1);
+		len = len + BUFF_SIZE;
+	}
+	if (r == 0)
+	{
+		free(current->s);
+		if (current != current->next)
+			{
+				tmp = current->next;
+				free(current);
+				current = tmp;
+			}
+		return (0);
+	}
+	return (-1);
+}
+
+
+
+
+
+/*int					get_next_line(const int fd, char **line)
 {
 	static t_endl	*current = NULL;
 	unsigned int	index;
@@ -134,7 +187,7 @@ int					get_next_line(const int fd, char **line)
 		return (0);
 	}
 	return (-1);
-}
+}*/
 
 /*
 int					get_next_line(const int fd, char **line)
