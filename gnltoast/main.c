25b67e5b67e9cda9b1ea2e72dbd79b913bdda2a0
Thibaut FONTAINE <tfontain@e2r3p2.42.fr>
2017-01-04 15:49:13 +0100

    test && main

--- gnltoast/main.c
+++ gnltoast/main.c
@@ -6,7 +6,7 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/16 00:18:49 by tfontain          #+#    #+#             */
-/*   Updated: 2016/12/31 02:16:07 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/01/04 01:45:40 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,57 +18,6 @@
 #include "../get_next_line.h"
 
 #define main1
-
-#ifdef main1
-int				main(int argc, char **argv) // test get_next_line
-{
-	if (argc != 2)
-		return (0);
-	char		*buffer;
-	int			fd;
-
-	fd = 0;
-	fd = open(*argv, O_RDONLY);
-	ft_putstr("fd : ");
-	ft_putnbr(fd);
-	ft_putchar('\n');
-	buffer = malloc(1);
-	buffer[0] = 0;
-	for (int k = 0 ; k < 10; ++k)
-	{
-		ft_putnbr(get_next_line(fd, &buffer));
-		ft_putchar('\n');
-		ft_putstr(buffer);
-		ft_putchar('\n');
-	}
-	close(fd);
-	return (0);
-}
-#endif
-
-#ifdef main2
-int				main() // test ft_find_nl
-{
-	ft_putnbr(ft_find_nl("0123456789\n0123456789"));
-	return (0);
-}
-#endif
-
-#ifdef main3
-int			main() // test ft_realloc_str
-{
-	char	*str;
-
-	str = malloc(6);
-	ft_strcpy(str, "yoyo\n");
-	str = ft_realloc_str(str, 11);
-	ft_strcpy(str, "123456789\n");
-	ft_putstr(str);
-	return (0);
-}
-#endif
-
-#ifdef main4
 
 void			ft_display_endl(t_endl *current)
 {
@@ -90,6 +39,71 @@ void			ft_display_endl(t_endl *current)
 	ft_putnbr(f_fd);
 }
 
+#ifdef main1
+int				main(int argc, char **argv) // test get_next_line
+{
+	if (argc != 2)
+		return (0);
+	char		*buffer;
+	int			fd;
+
+	fd = 0;
+	fd = open(argv[1], O_RDONLY);
+	ft_putstr(argv[1]);
+	ft_putstr("  fd : ");
+	ft_putnbr(fd);
+	ft_putchar('\n');
+
+	/*for (int k = 0 ; k < 10; ++k)
+	{*/
+		buffer = NULL;
+		get_next_line(fd, &buffer);
+		ft_putstr(buffer);
+		ft_putchar('\n');
+		free(buffer);
+
+		buffer = NULL;
+		get_next_line(fd, &buffer);
+		ft_putstr(buffer);
+		ft_putchar('\n');
+		free(buffer);
+	/*}*/
+	//char l; while (read(0, &l, 1) == 1); //
+	close(fd);
+	return (0);
+}
+#endif
+
+#ifdef main2
+int				main() // test ft_find_nl
+{
+	ft_putnbr(ft_find_nl("0123456789\n0123456789"));
+	return (0);
+}
+#endif
+
+#ifdef main3
+int			main() // test ft_realloc_str
+{
+	char	*str;
+
+	str = malloc(6);
+	ft_strcpy(str, "yoyo\n");
+	ft_putstr(str);
+	str = ft_realloc_str(str, 11);
+	ft_strcpy(str, "123456789\n");
+	ft_putstr(str);
+	str = ft_realloc_str(str, 25);
+	ft_strcpy(str, "toast\n");
+	ft_putstr(str);
+	char l;
+	while (read(0, &l, 1));
+	return (0);
+}
+#endif
+
+#ifdef main4
+
 int				main()
 {
 	t_endl		*current;
@@ -98,7 +112,9 @@ int				main()
 	current = ft_get_current(485, current);
 	current->s = malloc(6);
 	ft_strcpy(current->s, "toast");
-	current = ft_get_current(84, current);
+	current = ft_get_current(485, current);
+
+	/*current = ft_get_current(84, current);
 	current = ft_get_current(47, current);
 	current = ft_get_current(485, current);
 	current = ft_get_current(47, current);
@@ -106,7 +122,7 @@ int				main()
 	current->s = malloc(8);
 	ft_strcpy(current->s, "toaster");
 	current = ft_get_current(5, current);
-	current = ft_get_current(8, current);
+	current = ft_get_current(8, current);*/
 	ft_display_endl(current);
 	return (0);
 }
