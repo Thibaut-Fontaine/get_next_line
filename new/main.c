a70662dd77067d6a1c77b3f1155cf16bd1fbd749
Thibaut FONTAINE <tfontain@e2r11p22.42.fr>
2017-01-23 02:52:31 +0100

    erreur debile

--- new/main.c
+++ new/main.c
@@ -6,7 +6,7 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/20 01:27:06 by tfontain          #+#    #+#             */
-/*   Updated: 2017/01/20 12:09:56 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/01/23 02:52:14 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,21 @@
 int			main(int argc, char **argv)
 {
 	int		fd;
+	int		ret;
 	char	*line;
 
 	if (argc != 2)
 		return (EXIT_FAILURE);
 	fd = open(argv[1], O_RDONLY);
-	while (get_next_line(fd, &line) == 1)
+	while (1)
 	{
+		ret = get_next_line(fd, &line);
 		ft_putstr(line);
 		ft_putstr("\n");
 		free(line);
+		if (ret == 0 || ret == -1)
+			break ;
 	}
 	close(fd);
-
 	return (EXIT_SUCCESS);
 }
